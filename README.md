25d5811ef848309cf4fe881781a77463c29c1a0b
Son-Sumin <goldkitty007@gmail.com>
2023-05-18 17:01:33 +0900

    버전 조정

--- README.md
+++
@@ -1,2 +0,0 @@
-## helloheroku
-<br>
--- boardstudy/src/main/java/com/test/boardstudy/Repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.test.boardstudy.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.test.boardstudy.domain.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
-}
